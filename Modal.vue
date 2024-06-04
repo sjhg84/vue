0cd2e48fb89d77ab39bbe35243e4057a25b28863
Anosang <sjhg84@hotmail.com>
2024-06-04 13:20:15 +0800

    Add files via upload

---
+++ Modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>{{ header }}</h1>
+      <h2>{{ defaultTextline }}</h2>
+    </div>
+  </template>
+  
+  <script type="module">
+  export default {
+    data() {
+      return {
+        header: 'My header is here!' // Default header text
+      };
+    },
+    props: {
+      textline: {
+        type: String,
+        default: null // Explicitly define props with type and default
+      }
+    },
+    computed: {
+      defaultTextline() {
+        // Compute defaultTextline, return prop textline or a default string
+        return this.textline || 'My Textline is here!';
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add scoped CSS styles here */
+  </style>
